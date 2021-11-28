2c057f49d1b8aad879b004635f9304ce72615f98
Gabriel QUINT <gabriel.quint@epita.fr>
2021-11-28 22:03:55 +0100

    GUI change and thread change

--- graph.c
+++ graph.c
@@ -1,12 +1,13 @@
 #include <math.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <string.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#include <time.h>
 
-#include "validnext2.c"
+//#include "validnext2.c"
 
 // library installation : sudo apt-get install freeglut3
 //                        sudo apt-get install freeglut3-dev
@@ -14,92 +15,214 @@
 // compilation :  gcc main.c -o out -I /usr/include -lglut -lGL -lphthread
 // execution : ./out
 
-float x=0;
-float y=0;
+float x=0; //position of detection
+float y=0; //position of detection
+
+float r=0; //rayon of the wave
+
+int collision = 0; //if wave deetects somthing
+
+int channel=7; //channel scanned
+
+time_t t; //time displayed
 
 void initGraph()
 {
-	glClearColor(0,0,0,1);
-	glColor3f(0,0,1);
+    glClearColor(0,0,0,1);
+    glColor3f(0,0,1);
 }
 
 void initPoint()
 {
-	//glClearColor(0,0,0,1);
-	glColor3f(0,1,0);
+    //glClearColor(0,0,0,1);
+    glColor3f(0,1,0);
 }
 
-void update()
+void drawCircle(float cx, float cy, float r, int num_segments) {
+    glBegin(GL_LINE_LOOP);
+    for (int ii = 0; ii < num_segments; ii++)   {
+        float theta = 2.0f * 3.1415926f * (float)(ii) / (float)(num_segments);//get the current angle
+        float x = r * cosf(theta);//calculate the x component
+        float y = r * sinf(theta);//calculate the y component
+        glVertex2f(x + cx, y + cy);//output vertex
+    }
+    glEnd();
+}
+
+void updateRead()
 {
-	struct to_give received;
-	received=read();
-	x=received.resultatO-received.resultatE;
-	y=received.resultatS-received.resultatN;
-	if (x<=-0.5)
-		x=-0.5;
-	if (x>=0.5)
-		x=0.5;
-	if (y<=-0.5)
-		y=-0.5;
-	if (y>=0.5)
-		y=0.5;
-	glutPostRedisplay();
-	glutTimerFunc(3000,update,0);
+    //	struct to_give received;
+    //	received=read();
+    //	x=received.resultatO-received.resultatE;
+    //	y=received.resultatS-received.resultatN;
+    x=0.5;
+    y=0.5;
+    channel=7;
+    if (x<=-0.75)
+        x=-0.75;
+    if (x>=0.75)
+        x=0.75;
+    if (y<=-0.75)
+        y=-0.75;
+    if (y>=0.75)
+        y=0.75;
+    glutPostRedisplay();
+    glutTimerFunc(3000,updateRead,0);
+}
+
+void updateTime()
+{
+    //	struct to_give received;
+    //	received=read();
+    //	x=received.resultatO-received.resultatE;
+    //	y=received.resultatS-received.resultatN;
+    time(&t);
+
+    glutPostRedisplay();
+    glutTimerFunc(1000,updateTime,0);
+}
+
+void updateWave()
+{
+    //	struct to_give received;
+    //	received=read();
+    //	x=received.resultatO-received.resultatE;
+    //	y=received.resultatS-received.resultatN;
+
+    if (r>=2.5)
+        r=0;
+    else
+        r+=0.01;
+
+    //float val=sqrtf(x*x+y*y);
+    //printf("%f\n",val);
+    if (sqrtf(x*x+y*y)-0.05<=r && r<= sqrtf(x*x+y*y)+0.05)
+    {
+        collision=1;
+    }
+    else
+        collision=0;
+    glutPostRedisplay();
+    glutTimerFunc(10,updateWave,0);
 }
 
 void drawGraph()
 {
-	//printf("%f\n",f);
+    //printf("%f\n",f);
 
-	glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-	glPointSize(5);
+    //graph
+    glLineWidth(2);
 
-	glBegin(GL_LINES);
-	glVertex2f(-0.5,0);
-	glVertex2f(0.5,0);
-	glVertex2f(0,-0.5);
-	glVertex2f(0,0.5);
-	glEnd();
+    glBegin(GL_LINES);
+    glVertex2f(-0.75,0);
+    glVertex2f(0.75,0);
+    glVertex2f(0,-0.75);
+    glVertex2f(0,0.75);
+    glEnd();
+    //graph end
 
-	glColor3f(0,1,0);
-	glPointSize(10);
-	glBegin(GL_POINTS);
-	glVertex2f(x,y);
-	glEnd();
+    //circle
+    glLineWidth(1);
+    drawCircle(0,0,0.25,150);
+    drawCircle(0,0,0.45,150);
+    drawCircle(0,0,0.65,150);
+    //circle end 
 
-	glColor3f(0,0,1);
+    //wave
+    if (collision==0 || (x==0&&y==0))
+    {
+        glColor3f(1,0,0);
+        glLineWidth(1);
+    }
+    else
+    {
+        glColor3f(1,0,1);
+        glLineWidth(5);
+    }
+    drawCircle(0,0,r,150);
+    //wave end 
 
-	glFlush();
+    //detection
+    if (x==0 && y==0)
+        glColor3f(0,1,0);
+    else
+        glColor3f(1,0,0);
+
+    glPointSize(15);
+    glBegin(GL_POINTS);
+    glVertex2f(x,y);
+    glEnd();
+
+    glLineWidth(2);
+    glBegin(GL_LINES);
+    glVertex2f(0,0);
+    glVertex2f(x,y);
+    glEnd();
+    //detection end
+
+//channel
+    glColor3f(0,1,0);
+    char *chan=malloc(sizeof(char)*20);
+    char *channum=malloc(sizeof(char)*5);
+    strcpy(chan,"CHANNEL : ");
+    sprintf(channum,"%d",channel);
+    strcat(chan,channum);
+    int c = glutBitmapLength(GLUT_BITMAP_TIMES_ROMAN_24, chan);
+    glRasterPos2f(-0.98, 0.95);
+    int len = strlen(chan);
+    for (size_t i = 0; i < len; i++)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,chan[i]);
+    }
+    //channel end
+
+    //time
+    glColor3f(0,1,0);
+    int w = glutBitmapLength(GLUT_BITMAP_TIMES_ROMAN_24, ctime(&t));
+    glRasterPos2f(-1, -1);
+    len = strlen(ctime(&t));
+    for (size_t i = 0; i < len; i++)
+    {
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ctime(&t)[i]);
+    }
+    //time end
+    
+    glColor3f(0,0,1);
+    glFlush();
 }
 
 void drawPoint()
 {
-	//glClear(GL_COLOR_BUFFER_BIT);
+    //glClear(GL_COLOR_BUFFER_BIT);
 
-	glPointSize(20);
+    glPointSize(20);
 
-	glBegin(GL_POINTS);
-	glVertex2f(0.4,0.4);
-	glEnd();
-	glFlush();
+    glBegin(GL_POINTS);
+    glVertex2f(0.4,0.4);
+    glEnd();
+    glFlush();
 }
 
 
 int main (int argc, char *argv[])
 {
-	glutInit(&argc,argv);
+    glutInit(&argc,argv);
 
-	glutInitWindowPosition(200,100);
-	glutInitWindowSize(800,600);
+    glutInitWindowPosition(200,100);
+    //glutInitWindowSize(800,600);
+    glutInitWindowSize(1080,1080);
 
-	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-	glutCreateWindow("Radar");
-	initGraph();
-	glutDisplayFunc(drawGraph);
-	//initPoint();
-	//glutDisplayFunc(drawPoint);
-	update();
-	glutMainLoop();
-	return 0;
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutCreateWindow("Radar");
+    initGraph();
+    glutDisplayFunc(drawGraph);
+    //initPoint();
+    //glutDisplayFunc(drawPoint);
+    updateWave();
+    updateRead();
+    updateTime();
+    glutMainLoop();
+    return 0;
 }
--- validnext2.c
+++ validnext2.c
@@ -1,296 +1,258 @@
 #include <stdio.h>
 #include <pthread.h>
 
-#include<math.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 
 struct arg_struct
 {
-	double channel;
-	double id;
-	double resultat;
+    double channel;
+    double id;
+    double resultat;
 };
 
 struct to_give
 {
-	double resultatN;
-	double resultatS;
-	double resultatO;
-	double resultatE;
+    double resultatN;
+    double resultatS;
+    double resultatO;
+    double resultatE;
 };
 
 long countlines(char *filename)
 {
-	FILE* myfile = fopen(filename, "r");
-	long ch, number_of_lines = 0;
-	do
-	{
-		ch = fgetc(myfile);
-		if(ch == '\n')
-			number_of_lines++;
-	}
-	while (ch != EOF);
-	if(ch != '\n' && number_of_lines != 0)
-		number_of_lines++;
-	fclose(myfile);
-	return number_of_lines-1;
+    FILE* myfile = fopen(filename, "r");
+    long ch, number_of_lines = 0;
+    do
+    {
+        ch = fgetc(myfile);
+        if(ch == '\n')
+            number_of_lines++;
+    }
+    while (ch != EOF);
+    if(ch != '\n' && number_of_lines != 0)
+        number_of_lines++;
+    fclose(myfile);
+    return number_of_lines-1;
 }
 
 long countrows(char *filename)
 {
-	FILE* myfile = fopen(filename, "r");
-	long ch, number_of_rows = 0;
-	do
-	{
-		ch = fgetc(myfile);
-		if(ch == ',')
-			number_of_rows++;
-	}
-	while (ch != '\n');
-	fclose(myfile);
+    FILE* myfile = fopen(filename, "r");
+    long ch, number_of_rows = 0;
+    do
+    {
+        ch = fgetc(myfile);
+        if(ch == ',')
+            number_of_rows++;
+    }
+    while (ch != '\n');
+    fclose(myfile);
 
-	number_of_rows++;
-	return number_of_rows;
+    number_of_rows++;
+    return number_of_rows;
 }
-/*
-void setValue(struct arg_struct *arg,double id,double value)
-{
-
-	struct arg_struct *args=(struct arg_struct *)arg;
-	double id=args->id;
-	double resultatN=args->resultatN;
-	double resultatS=args->resultatS;
-	double resultatO=args->resultatO;
-	double resultatE=args->resultatE;
-	if (id==0)
-		args->resultatN=value;
-	else if (id==1)
-		args->resultatS=value;
-	else if (id==2)
-		args->resultatO=value;
-	else if (id==3)
-		args->resultatE=value;
-	return NULL;
-}
-*/
 
 static void *read_db(void* arguments)
 {
-	struct arg_struct *args=(struct arg_struct *)arguments;
-	double channel=args->channel;
-	double id=args->id;
-	double resultat=args->resultat;
-	double max=-100;
+    struct arg_struct *args=(struct arg_struct *)arguments;
+    double channel=args->channel;
+    double id=args->id;
+    double max=-100;
 
+    double range_inf=446006250.0+((channel-1)*12500.0)-300.0;
+    double range_sup=446006250.0+((channel-1)*12500.0)+300.0;
 
-	//printf("listening to %lf\n",446006250.0+((channel-1)*12500.0));
+    char *filename=malloc(sizeof(char)*50);
+    strcpy(filename,"results");
+    char *idn=malloc(sizeof(char)*20);
+    sprintf(idn,"%ld",(long)id);
+    strcat(filename,idn);
+    free(idn);
+    strcat(filename,".csv");
 
-	double range_inf=446006250.0+((channel-1)*12500.0)-300.0;
-	double range_sup=446006250.0+((channel-1)*12500.0)+300.0;
-	//printf("ri = %lf , rs = %lf\n",range_inf,range_sup);
+    char *command=(char *)malloc(sizeof(char)*200);
 
-	char *filename=NULL;
-	filename=(char *)malloc(sizeof(char)*50);
-	strcpy(filename,"results");
-	char *idn=malloc(sizeof(char)*20);
-	sprintf(idn,"%ld",(long)id);
-	strcat(filename,idn);
-	free(idn);
-	strcat(filename,".csv");
+    strcpy(command,"rtl_power -d ");
 
-	char *command=NULL;
-	command=(char *)malloc(sizeof(char)*200);
+    char *id1=malloc(sizeof(char)*20);
+    sprintf(id1,"%ld",(long)id);
+    strcat(command,id1);
+    free(id1);
 
-	strcpy(command,"rtl_power -d ");
+    strcat(command," -f ");
 
-	char *id1=malloc(sizeof(char)*20);
-	sprintf(id1,"%ld",(long)id);
-	strcat(command,id1);
-	free(id1);
+    char *b1=malloc(sizeof(char)*20);
+    sprintf(b1,"%ld",(long)range_inf);
+    strcat(command,b1);
+    free(b1);
 
-	strcat(command," -f ");
+    strcat(command,":");
 
-	char *b1=malloc(sizeof(char)*20);
-	sprintf(b1,"%ld",(long)range_inf);
-	strcat(command,b1);
-	free(b1);
+    char *b2=malloc(sizeof(char)*20);
+    sprintf(b2,"%ld",(long)range_sup);
+    strcat(command,b2);
+    free(b2);
 
-	strcat(command,":");
+    strcat(command,":1 -g 0 -c 0.99 -i 1 -1 > ");
+    strcat(command,filename);
 
-	char *b2=malloc(sizeof(char)*20);
-	sprintf(b2,"%ld",(long)range_sup);
-	strcat(command,b2);
-	free(b2);
+    printf("%s\n",command);
+    system(command);
 
-	strcat(command,":1 -g 0 -c 0.99 -i 1 -1 > ");
-	strcat(command,filename);
+    free(command);
 
-	printf("%s\n",command);
-	system(command);
+    long result;
+    FILE* f = fopen(filename, "r");
 
-	free(command);
+    long size;
+    if (f!=NULL)
+    {
+        fseek(f,0,SEEK_END);
+        size=ftell(f);
+        if (size<=1)
+        {
+            printf("================    antenna %lf not connected    =====\n",id);
+            fclose(f);
+            remove(filename);
+            
+            free(filename);
+            //free(f);
+            args->resultat=0;
+            return NULL;
+        }
+    }
+    else
+    {
+        fclose(f);
+        remove(filename);
+        free(filename);
+        //free(f);
+        args->resultat=0;
+        return NULL;
+    }
 
-	long result;
-	FILE* f = fopen(filename, "r");
+    fclose(f);
 
-	long size;
-	if (f!=NULL)
-	{
-		fseek(f,0,SEEK_END);
-		size=ftell(f);
-		if (size<=1)
-		{
-			printf("================    antenna %lf not connected    =====\n",id);
-			fclose(f);
-			remove(filename);
-			free (filename);
-			if (max<-49)
-			{
-				args->resultat=0;
-				return NULL;
-			}
-		}
-	}
-	else
-	{
-		fclose(f);
-		remove(filename);
-		if (max<-49)
-		{
-			args->resultat=0;
-			return NULL;
-		}
-	}
-		fclose(f);
+    long l=countlines(filename);
+    long r=countrows(filename);
 
+    char *str =calloc(2048,sizeof(long));
+    double *tab =calloc(l*r+1, sizeof(double));
+    f = fopen(filename, "r");
+    long n=0;
+    do
+    {
+        result = fscanf(f, "%127[^;, \n]", str);
 
-	long l=countlines(filename);
-	long r=countrows(filename);
-	//printf("rows = %d, lines = %d\n",r,l);
+        if(result == 0)
+        {
+            result = fscanf(f, "%*c");
+        }
+        else
+        {
+            if (strcmp(str,"nan")!=0)
+            {
+                tab[n]=atof(str);
+                if (tab[n]>max && n>=6)
+                {
+                    max=tab[n];
+                }
+            }
+            else
+            {
+                tab[n]=-100;
+            }
+            n++;
 
-	char *str =calloc(2048,sizeof(long));
-	double *tab =calloc(l*r+1, sizeof(double));
-	f = fopen(filename, "r");
+        }
+    } while(result != EOF);
 
-	//double max=-100;
-	long imax;
-	long n=0;
-	do
-	{
-		result = fscanf(f, "%127[^;, \n]", str);
+    free(tab);
+    free(str);
+    fclose(f);
+    remove(filename);
+    //free(f);
+    free(filename);
+    printf("\n\n\nmax=%lf\n\n\n",max);
 
-		if(result == 0)
-		{
-			result = fscanf(f, "%*c");
-		}
-		else
-		{
-			if (strcmp(str,"nan")!=0)
-			{
-				tab[n]=atof(str);
-				if (tab[n]>max && n>=6)
-				{
-					imax=n;
-					max=tab[n];
-				}
-			}
-			else
-			{
-				tab[n]=-100;
-			}
-			n++;
+    if (max<-49)
+        args->resultat=0;
+    else
+        args->resultat=max;
 
-		}
-	} while(result != EOF);
-
-	//printf("max value = %lf\n",max);
-	//printf("max value relative index = %ld\n",imax-6);
-
-
-	//printf("frequency : %.2lf hz | strength : %.2lf dB | rssi : %.2lf %\n",(tab[2]+(tab[4]*(double)(imax-6))),tab[6+(imax-6)],(tab[6+(imax-6)]+100));
-	free(tab);
-	free(str);
-	fclose(f);
-		remove(filename);
-	printf("\n\n\nmax=%lf\n\n\n",max);
-
-	/*
-	   double *pmax;
-	   pmax=&max;
-	   if (max<49)
-	   pthread_exit(pmax);
-	   pthread_exit(pmax);
-	   */
-	if (max<-49)
-		args->resultat=0;
-	else
-		args->resultat=max;
-
-	return NULL;
+    return NULL;
 }
 
 
 struct to_give read(void)
 {
-	int ret = 0;
-	pthread_t thread_read0;
-	pthread_t thread_read1;
-	pthread_t thread_read2;
-	pthread_t thread_read3;
+    //int ret;
+    pthread_t thread_read0;
+    pthread_t thread_read1;
+    pthread_t thread_read2;
+    pthread_t thread_read3;
 
-	struct arg_struct argsN;
-	argsN.channel=7;
-	argsN.id=0;
-	argsN.resultat=0;
+    struct arg_struct argsN;
+    argsN.channel=7;
+    argsN.id=0;
+    argsN.resultat=0;
 
-	struct arg_struct argsS;
-	argsS.channel=7;
-	argsS.id=1;
-	argsS.resultat=0;
+    struct arg_struct argsS;
+    argsS.channel=7;
+    argsS.id=1;
+    argsS.resultat=0;
 
-	struct arg_struct argsO;
-	argsO.channel=7;
-	argsO.id=2;
-	argsO.resultat=0;
+    struct arg_struct argsO;
+    argsO.channel=7;
+    argsO.id=2;
+    argsO.resultat=0;
 
-	struct arg_struct argsE;
-	argsE.channel=7;
-	argsE.id=3;
-	argsE.resultat=0;
+    struct arg_struct argsE;
+    argsE.channel=7;
+    argsE.id=3;
+    argsE.resultat=0;
 
-	//void *out_void=(void*)malloc(sizeof(double)*2);
-	//double out;
-	ret = pthread_create (&thread_read0,NULL,&read_db,(void *)&argsN);
-	ret = pthread_create (&thread_read1,NULL,&read_db,(void *)&argsS);
-	ret = pthread_create (&thread_read2,NULL,&read_db,(void *)&argsO);
-	ret = pthread_create (&thread_read3,NULL,&read_db,(void *)&argsE);
-	pthread_join (thread_read0, NULL);
-	pthread_join (thread_read1, NULL);
-	pthread_join (thread_read2, NULL);
-	pthread_join (thread_read3, NULL);
-	//out=*(double *)out_void;
-	//free(out_void);
+    //void *out_void=(void*)malloc(sizeof(double)*2);
+    //double out;
+    pthread_create (&thread_read0,NULL,&read_db,(void *)&argsN);
+    pthread_create (&thread_read1,NULL,&read_db,(void *)&argsS);
+    pthread_create (&thread_read2,NULL,&read_db,(void *)&argsO);
+    pthread_create (&thread_read3,NULL,&read_db,(void *)&argsE);
+    pthread_join (thread_read0, NULL);
+    pthread_join (thread_read1, NULL);
+    pthread_join (thread_read2, NULL);
+    pthread_join (thread_read3, NULL);
+    //out=*(double *)out_void;
+    //free(out_void);
 
-	printf("\n\n\n%lf\n\n\n\n",argsN.resultat);
-	printf("\n\n\n%lf\n\n\n\n",argsS.resultat);
-	printf("\n\n\n%lf\n\n\n\n",argsO.resultat);
-	printf("\n\n\n%lf\n\n\n\n",argsE.resultat);
+    printf("\n\n\n%lf\n\n\n\n",argsN.resultat);
+    printf("\n\n\n%lf\n\n\n\n",argsS.resultat);
+    printf("\n\n\n%lf\n\n\n\n",argsO.resultat);
+    printf("\n\n\n%lf\n\n\n\n",argsE.resultat);
 
 
-	struct to_give final_result;
-	final_result.resultatN=argsN.resultat;
-	final_result.resultatS=argsS.resultat;
-	final_result.resultatO=argsO.resultat;
-	final_result.resultatE=argsE.resultat;
+    struct to_give final_result;
+    final_result.resultatN=argsN.resultat;
+    final_result.resultatS=argsS.resultat;
+    final_result.resultatO=argsO.resultat;
+    final_result.resultatE=argsE.resultat;
 
-	return final_result;
+    return final_result;
 }
 
 /*
 int main(void)
 {
-	read();
-	return 0;
+    read();
+    read();
+    read();
+    read();
+    read();
+    read();
+    read();
+    read();
+    return 0;
 }
 */
