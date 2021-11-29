59b0c3d868fc0602a83b81e577d9eb13b2cb9141
Gabriel QUINT <gabriel.quint@epita.fr>
2021-11-29 12:28:35 +0100

    architecture redone

--- configure.sh
+++ configure.sh
@@ -2,4 +2,4 @@
 
 mkdir -p logs
 
-gcc graph.c -g -I /usr/include -lglut -lm -lGL -lpthread -o rdf
+gcc source/main.c -g -I /usr/include -lglut -lm -lGL -lpthread -o rdf
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+sudo apt-get install freeglut3
+sudo apt-get install freeglut3-dev
